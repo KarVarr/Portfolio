58e318a495319fe327d15e394f001afb78ac4700
Karen Vardanian <karenvardanian@MacBook-Air-Karen.local>
2022-06-27 18:01:11 +0300

    add responsive and burger menu

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ const portfolio = document.querySelector('#portfolio');
 const education = document.querySelector('#education');
 const contacts = document.querySelector('#contacts');
 const footer = document.querySelector('#footer');
-const header = document.querySelector('.header')
+const header = document.querySelector('.header');
 const nav = document.querySelector('.nav_items');
 
+//burgerMenu
+const homeMenu = document.querySelector('#homeMenu');
+const aboutMenu = document.querySelector('#aboutMenu');
+const skillMenu = document.querySelector('#skillMenu');
+const portfolioMenu = document.querySelector('#portfolioMenu');
+const educationMenu = document.querySelector('#educationMenu');
+const contactsMenu = document.querySelector('#contactsMenu');
+
+//section All
 const aboutSection = document.querySelector('.aboutme');
 const skillsSection = document.querySelector('.skills');
 const portfolioSection = document.querySelector('.portfolio');
@@ -18,6 +27,10 @@ const container = document.querySelector('.container');
 const toggleDayNight = document.querySelector('#toggle');
 
 const allSection = document.querySelectorAll('section');
+
+const btnBurger = document.querySelector('#burger');
+const navBurger = document.querySelector('.nav__burger');
+const bodyLock = document.body;
 
 //scroll-------
 
@@ -72,6 +85,7 @@ toggleDayNight.addEventListener('click', () => {
 });
 
 //sticky nav------
+
 // const initialCoords = aboutSection.getBoundingClientRect();
 // console.log(initialCoords);
 
@@ -82,15 +96,14 @@ toggleDayNight.addEventListener('click', () => {
 //   else nav.classList.remove('stiky');
 // });
 
-const stickyNav = function(entries) {
+const stickyNav = function (entries) {
   const [entry] = entries;
   console.log(entry);
 
-  if(!entry.isIntersecting) {
-    nav.classList.add('stiky')
+  if (!entry.isIntersecting) {
+    nav.classList.add('stiky');
   } else nav.classList.remove('stiky');
-
-}
+};
 
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
@@ -98,15 +111,12 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   //rootMargin: '-90px'
 });
 
-headerObserver.observe(header)
-
-
+headerObserver.observe(header);
 
 //Reveal section--------------
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
@@ -121,4 +131,60 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 allSection.forEach(section => {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
+});
+
+//BURGER NAV
+
+btnBurger.addEventListener('click', () => {
+  if (btnBurger.checked) {
+    navBurger.classList.add('activeBurgerMenu');
+    bodyLock.classList.add('lock');
+  } else {
+    navBurger.classList.remove('activeBurgerMenu');
+    bodyLock.classList.remove('lock');
+  }
+});
+
+//navigationChecked
+
+homeMenu.addEventListener('click', () => {
+  btnBurger.checked = false;
+  navBurger.classList.remove('activeBurgerMenu');
+  bodyLock.classList.remove('lock');
+  window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+});
+
+aboutMenu.addEventListener('click', () => {
+  btnBurger.checked = false;
+  navBurger.classList.remove('activeBurgerMenu');
+  bodyLock.classList.remove('lock');
+  aboutSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+skillMenu.addEventListener('click', () => {
+  btnBurger.checked = false;
+  navBurger.classList.remove('activeBurgerMenu');
+  bodyLock.classList.remove('lock');
+  skillsSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+portfolioMenu.addEventListener('click', () => {
+  btnBurger.checked = false;
+  navBurger.classList.remove('activeBurgerMenu');
+  bodyLock.classList.remove('lock');
+  portfolioSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+educationMenu.addEventListener('click', () => {
+  btnBurger.checked = false;
+  navBurger.classList.remove('activeBurgerMenu');
+  bodyLock.classList.remove('lock');
+  educationSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+contactsMenu.addEventListener('click', () => {
+  btnBurger.checked = false;
+  navBurger.classList.remove('activeBurgerMenu');
+  bodyLock.classList.remove('lock');
+  footer.scrollIntoView({ behavior: 'smooth' });
 });
